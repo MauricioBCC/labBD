2e8dd125ef5a46f64957a471cdf006b61af0226b
mauricio <mauriciolacardoso@yahoo.com.br>
2018-04-01 15:08:20 -0300

    incluidas todas tabelas

--- exercicio4CriarBD/criaTabelas.sql
+++ exercicio4CriarBD/criaTabelas.sql
@@ -1,19 +1,32 @@
 CREATE DOMAIN tipo_cpf AS CHAR(11);
 
-CREATE TABLE professor (
-	nusp int PRIMARY KEY,
-	cpf tipo_cpf NOT NULL,
-	nome NOT NULL,
-	sala VARCHAR(50) NOT NULL
+CREATE TABLE titulo_professor (
+	titulo VARCHAR(50),
+	nusp_prof INT,
+	PRIMARY KEY(titulo, nusp_prof)
+	FOREIGN KEY(nusp_prof) REFERENCES professor(nusp),
+	CHECK (titulo IN ('Doutorado','Mestrado','Especialização','Bacharelado',
+		'Licenciatura','Graduação Tecnológica'))
+);
 
+CREATE TABLE professor (
+	nusp INT PRIMARY KEY,
+	cpf tipo_cpf NOT NULL,
+	nome VARCHAR(50)NOT NULL,
+	sala VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE aluno (
+	id_aluno INT NOT NULL PRIMARY KEY
 );
 
 CREATE TABLE aluno_regular (
-	nusp int PRIMARY KEY,
+	nusp INT PRIMARY KEY,
 	cpf tipo_cpf NOT NULL,
 	nome VARCHAR(50) NOT NULL,
-	nusp_orientador int NOT NULL,
-	id_aluno int NOT NULL,
+	curso VARCHAR(50) DEFAULT 'Bacharelado em Ciência da Computação'
+	nusp_orientador INT NOT NULL,
+	id_aluno INT NOT NULL,
 	FOREIGN KEY(nusp_orientador) REFERENCES professor(nusp),
 	FOREIGN KEY(id_aluno) REFERENCES aluno(id_aluno)
 );
@@ -21,11 +34,42 @@ CREATE TABLE aluno_regular (
 CREATE TABLE aluno_especial (
 	email VARCHAR(50) NOT NULL,
 	nome VARCHAR(50) NOT NULL,
-	id_aluno int NOT NULL,
-	FOREIGN KEY(id_aluno) REFERENCES aluno(id_aluno),
+	id_aluno INT NOT NULL,
+	FOREIGN KEY(id_aluno) REFERENCES aluno(id_aluno)
 );
 
-CREATE TABLE aluno (
-	id_aluno int NOT NULL PRIMARY KEY
+CREATE TABLE disciplina (
+	codigo_disciplina INT PRIMARY KEY,
+	nome VARCHAR(50)
 );
 
+CREATE TABLE turma (
+	id_turma INT,
+	semestre INT,
+	ano INT,
+	codigo_disciplina INT,
+	id_aluno INT NOT NULL,
+	nota INT NOT NULL CHECK (nota >= 0 AND nota <= 10),
+	frequencia INT NOT NULL,
+	PRIMARY KEY(id_turma, semestre, ano, codigo_disciplina),
+	FOREIGN KEY(codigo_disciplina) REFERENCES disciplina(codigo_disciplina),
+	FOREIGN KEY(id_aluno) REFERENCES aluno(id_aluno)
+);
+
+CREATE TABLE ministra (
+	nusp_prof INT,
+	semestre INT,
+	ano INT,
+	codigo_disciplina INT,
+	id_turma INT,
+	PRIMARY KEY(nusp_prof, semestre, ano, codigo_disciplina, id_turma),
+	FOREIGN KEY(id_turma, semestre, ano, codigo_disciplina) REFERENCES turma(id_turma, semestre, ano, codigo_disciplina),
+	FOREIGN KEY(nusp_prof) REFERENCES professor(nusp),
+	FOREIGN KEY(codigo_disciplina) REFERENCES disciplina(codigo_disciplina)
+);
+
+CREATE TABLE disc_pre_requisito (
+	id_disciplina INT,
+	id_disciplina_requisito INT,
+	PRIMARY KEY(id_disciplina, id_disciplina_requisito)
+);
